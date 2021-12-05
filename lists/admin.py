a358c8b8a171c9aa491f1733317003dc0cae9ce1
blingblin-g <luvbliny@gmail.com>
2021-12-05 18:07:10 +0900

    feat : lists admins

--- lists/admin.py
+++ lists/admin.py
@@ -6,4 +6,6 @@ from . import models
 @admin.register(models.List)
 class ListAdmin(admin.ModelAdmin):
 
-    pass
+    list_display = ("name", "user", "count_rooms")
+
+    search_filter = ("name",)
