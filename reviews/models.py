6893ef4f62eec9403f7e0c4a1a06bc075bb599e4
blingblin-g <luvbliny@gmail.com>
2021-12-05 17:54:48 +0900

    feat : ratings average in reviews

--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from core import models as core_models
 
+
 # Create your models here.
 class Review(core_models.TimeStampedModel):
     """Review Model Definition"""
@@ -22,3 +23,16 @@ class Review(core_models.TimeStampedModel):
 
     def __str__(self):
         return f"{self.review} - {self.room}"
+
+    def rating_average(self):
+        avg = (
+            self.accuracy
+            + self.communication
+            + self.cleanliness
+            + self.location
+            + self.check_in
+            + self.value
+        ) / 6
+        return round(avg, 2)
+
+    rating_average.short_description = "Avg."
