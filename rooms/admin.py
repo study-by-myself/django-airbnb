5f2055104bedb588ac628461b4d0811ac9ecdd08
blingblin-g <luvbliny@gmail.com>
2021-12-09 18:20:46 +0900

    feat : init view

--- rooms/admin.py
+++ rooms/admin.py
@@ -100,7 +100,7 @@ class RoomAdmin(admin.ModelAdmin):
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    """Phot Admin Definition"""
+    """Photo Admin Definition"""
 
     list_display = ("__str__", "get_thumbnail")
 
