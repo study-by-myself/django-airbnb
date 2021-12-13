e8029e61e3a3b03ae8cd6cfe6ffdce5ce416c193
blingblin-g <luvbliny@gmail.com>
2021-12-13 18:42:45 +0900

    feat : templates init

--- rooms/views.py
+++ rooms/views.py
@@ -6,4 +6,4 @@ from . import models
 
 def all_rooms(request):
     all_rooms = models.Room.objects.all()
-    return render(request, "all_rooms.html", context={"rooms": all_rooms})
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
