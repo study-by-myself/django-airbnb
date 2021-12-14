c258b7b4d9eeae1f15520ae93835fa1a1db423e4
blingblin-g <luvbliny@gmail.com>
2021-12-14 15:50:03 +0900

    feat : room detail view with cbv

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from django.utils import timezone
 from django.urls import reverse
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.http import Http404
 from django.shortcuts import redirect, render
 from . import models
@@ -23,9 +23,7 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-    except models.Room.DoesNotExist:
-        raise Http404()
-    return render(request, "rooms/detail.html", context={"room": room})
+class RoomDetail(DetailView):
+    """Room Detail description"""
+
+    model = models.Room
