2e5fbb848ce98d6f2efec1897c10b4cd642cdda0
blingblin-g <luvbliny@gmail.com>
2021-12-14 11:53:21 +0900

    feat : exception handling with redirect

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import redirect, render
+from django.core.paginator import EmptyPage, Paginator
 from . import models
 
 # Create your views here.
@@ -10,5 +10,8 @@ def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", context={"page": rooms})
+    try:
+        rooms = paginator.page(page)
+        return render(request, "rooms/home.html", context={"page": rooms})
+    except EmptyPage:
+        return redirect("/")
